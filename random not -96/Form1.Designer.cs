51fe83effabda023748c869b1d77582af69fec3e
hilal <130781977+mshelovee@users.noreply.github.com>
2023-10-24 14:16:03 +0300

    Add files via upload

---
+++ random not -96/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace random_not__96
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNotlar = new System.Windows.Forms.ListBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbNotlar
+            // 
+            this.lbNotlar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbNotlar.FormattingEnabled = true;
+            this.lbNotlar.ItemHeight = 16;
+            this.lbNotlar.Location = new System.Drawing.Point(12, 97);
+            this.lbNotlar.Name = "lbNotlar";
+            this.lbNotlar.Size = new System.Drawing.Size(200, 292);
+            this.lbNotlar.TabIndex = 0;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(12, 33);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(200, 49);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = " EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(229, 431);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.lbNotlar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbNotlar;
+        private System.Windows.Forms.Button btnEkle;
+    }
+}
+
---
+++ random not -96/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace random_not__96
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            lbNotlar.Items.Clear();
+            Random rnd = new Random();
+
+            for (int i=0; i<=14; i++)
+            {
+                int puan = rnd.Next(0,100);
+
+                lbNotlar.Items.Add(puan);
+            }
+
+        }
+    }
+}
